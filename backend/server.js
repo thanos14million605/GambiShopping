2a69b4cd873972a1c9adc9860ddd054e8e60ff5a
Ebrima Gajaga <gajaga.dev@gmail.com>
2025-10-02 12:30:43 +0530

    Updated server.js

--- backend/server.js
+++ backend/server.js
@@ -32,8 +32,6 @@ if (process.env.NODE_ENV === "production") {
 
 const PORT = process.env.PORT || 5000;
 
-connectToDB(process.env.MONGO_URI);
-
 const server = app.listen(PORT, () => {
   console.log(`App is running on port ${PORT}`);
 });
